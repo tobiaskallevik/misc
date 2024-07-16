84f147e678371fc806101519171494dc93732e3f
Tobias Fidje Kallevik <tobiaskallevik@gmail.com>
2024-07-16 21:37:53 +0200

    Companies and image fetch change

--- igbdLocal/getGamesFromAPI.py
+++ igbdLocal/getGamesFromAPI.py
@@ -48,24 +48,48 @@ def fetch_igdb_games(auth_token, game_id):
     return response.json()
 
 
+def fetch_companies(auth_token, involved_companies_id):
+    url = "https://api.igdb.com/v4/involved_companies"
+    headers = {
+        "Accept": "application/json",
+        "Client-ID": client_id,
+        "Authorization": "Bearer " + auth_token,
+        "Content-Type": "text/plain"
+    }
+    data = ('fields company.name, company.slug, company.description, company.logo, '
+            'company.start_date;'
+            'limit 500;'
+            'sort id asc;'
+            'where id >' + str(involved_companies_id) + ';')
+    response = requests.post(url, headers=headers, data=data)
+
+    # If the response is 401, get a new auth token and recursively call the function
+    if response.status_code == 401:
+        auth_token = get_auth_token()
+        return fetch_igdb_games(auth_token, involved_companies_id)
+
+    return response.json()
+
+
 # Create a json file
-def create_json():
-    with open("games2.json", "w") as file:
+def create_json(file_name):
+    with open(file_name, "w") as file:
         file.write("[]")
 
 
 # Write to the json file
-def write_to_json(data):
-    with open("games2.json", "r") as file:
+def write_to_json(data, file_name):
+    with open(file_name, "r") as file:
         games = json.load(file)
         games.extend(data)  # Extend the list with the new data
-    with open("games2.json", "w") as file:
-        json.dump(games, file, indent=4)  # Optional: add indent for better readability
+    with open(file_name, "w") as file:
+        json.dump(games, file, indent=4)
 
 
 # Main function
-def main():
-    create_json()
+def initiate_games():
+    file_name = "games2.json"
+    create_json(file_name)
     auth_token = get_auth_token()
     game_id = 160499
 
@@ -75,10 +99,52 @@ def main():
             print("Got all games")
             break
 
-        write_to_json(response)
+        write_to_json(response, file_name)
         print("Got games from id " + str(game_id), "to " + str(game_id + 500))
         game_id += 500
 
 
+def initiate_companies():
+    file_name = "companies.json"
+    create_json(file_name)
+    auth_token = get_auth_token()
+    involved_companies_id = 0
+    companies = []
+
+    while True:
+        response = fetch_companies(auth_token, involved_companies_id)
+        if len(response) == 0:
+            print("Got all companies")
+            break
+
+        for item in response:
+            company_id = item.get("id", "N/A")
+            company = item.get("company", {})
+            name = company.get("name", "N/A")
+            slug = company.get("slug", "N/A")
+            description = company.get("description", "N/A")
+            logo = company.get("logo", "N/A")
+            start_date = company.get("start_date", "N/A")
+
+            cut_down_response = {
+                "id": company_id,
+                "name": name,
+                "slug": slug,
+                "description": description,
+                "logo": logo,
+                "start_date": start_date
+            }
+
+            involved_companies_id = company_id
+
+            companies.append(cut_down_response)
+
+        print("Got companies to id " + str(involved_companies_id))
+        involved_companies_id += 1
+
+    write_to_json(companies, file_name)
+
+
 if __name__ == "__main__":
-    main()
+    # initiate_games()
+    initiate_companies()
--- igbdLocal/getImageFromGiantBomb.py
+++ igbdLocal/getImageFromGiantBomb.py
@@ -1,6 +1,10 @@
+import math
 import os
+import random
 import sqlite3
 import json
+import time
+
 import requests
 from dotenv import load_dotenv
 
@@ -19,7 +23,8 @@ def get_game_info(game_name):
         "field_list": "image"
     }
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/58.0.3029.110 Safari/537.3"
     }
 
     response = requests.get(base_url, params=query_params, headers=headers)
@@ -30,8 +35,6 @@ def get_game_info(game_name):
     # Error handling
     try:
         response.raise_for_status()
-
-    # Extraxt the medium image url
     except requests.exceptions.HTTPError as err:
         print(f"HTTP Error: {err}")
         return None
@@ -44,22 +47,49 @@ def create_json():
         file.write("[]")
 
 
-def write_to_json(id, image_url):
+def write_to_json(game_id, game_name, image_url):
     with open("gamesImages.json", "r") as file:
         games = json.load(file)
-        games.append({"id": id, "image_url": image_url})
+        games.append({"game_id": game_id, "game_name": game_name, "image_url": image_url})
     with open("gamesImages.json", "w") as file:
         json.dump(games, file, indent=4)
 
 
-# Gets the game name and game id from the database
+# Gets the game name and game id from the database.
+# Then gets the image url from the Giant Bomb API.
+# Finally, writes the game id, name and image url to a JSON file.
 def main():
-    conn = sqlite3.connect('games.db')  # Connect to the database
-    cursor = conn.cursor()  # Create a cursor object
+    conn = sqlite3.connect('games.db')
+    cursor = conn.cursor()
+    last_game_id = 0
 
-    for row in cursor.execute("SELECT id, name FROM games"):
+    # Get the id last game from the last element in the gamesImages.json file
+    try:
+        with open("gamesImages.json", "r") as file:
+            games = json.load(file)
+            if games:  # Check if the list is not empty
+                last_game_id = games[-1]["game_id"]
+            else:
+                print("gamesImages.json is empty. Starting from the beginning.")
+    except FileNotFoundError:
+        print("gamesImages.json not found. Creating a new file.")
+        create_json()
+
+    for row in cursor.execute("SELECT id, name FROM games WHERE id > ?", (last_game_id,)):
         game_id, game_name = row
-        print(f"Getting image for {game_name}")
+        response = get_game_info(game_name)
+
+        if response is not None:
+            image_url = response["results"][0]["image"]["medium_url"]
+            write_to_json(game_id, game_name, image_url)
+            print("Got image for", game_name)
+
+        else:
+            print("No image found for", game_name)
+
+        time.sleep(round(random.uniform(2, 4), 3))
+
+    conn.close()
 
 
 if __name__ == "__main__":
